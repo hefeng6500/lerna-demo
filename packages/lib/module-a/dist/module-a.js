49a876b489eef0152c657ce7d157e820a357fe5a
hefeng6500 <2443992009@qq.com>
2020-12-03 22:03:01 +0800

    fix: 修改包路径

--- packages/lib/module-a/dist/module-a.js
+++ packages/lib/module-a/dist/module-a.js
@@ -1,9 +1,16 @@
 'use strict';
 
+Object.defineProperty(exports, '__esModule', { value: true });
+
 var _ = require('lodash');
 
 function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
 
 var ___default = /*#__PURE__*/_interopDefaultLegacy(_);
 
-console.log(___default['default']);
+function testA() {
+  console.log('AAAAAAAAAAAA');
+  console.log(___default['default']);
+}
+
+exports.testA = testA;
---
+++ packages/lib/module-a/lib/module-a.js
@@ -0,0 +1,7 @@
+import _ from 'lodash';
+
+export default function testA() {
+  return {
+    lodash: _
+  };
+}
